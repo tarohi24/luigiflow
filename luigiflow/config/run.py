42cbf99365f1b40e3b658e1ba6a77beb463ae326
Wataru Hirota <wataru@whiro.me>
2022-03-28 08:01:41 +0900

    Allow arbitrary string as `mlflow_uri`

--- luigiflow/config/run.py
+++ luigiflow/config/run.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel, HttpUrl, Field
 
 
 class RunnerConfig(BaseModel):
-    mlflow_tracking_uri: HttpUrl
+    mlflow_tracking_uri: str
     config_path: Union[str, Path]  # Don't use `PathLike` because `BaseModel` doesn't have a validator for it
     use_local_scheduler: bool = True
     create_experiment_if_not_existing: bool = False
