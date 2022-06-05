d2cf294c5d2e8b74b50ae2b71d3e22201610f150
Wataru Hirota <wataru@whiro.me>
2022-06-06 02:35:12 +0900

    black

--- luigiflow/utils/testing.py
+++ luigiflow/utils/testing.py
@@ -47,9 +47,7 @@ def _await_server_up_or_die(port, timeout=60):
             print("Server not yet up, waiting...")
             time.sleep(0.5)
     if not connected:
-        raise Exception(
-            "Failed to connect on %s:%s after %s seconds" % (LOCALHOST, port, timeout)
-        )
+        raise Exception("Failed to connect on %s:%s after %s seconds" % (LOCALHOST, port, timeout))
     print("Server is up on %s:%s!" % (LOCALHOST, port))
 
 
